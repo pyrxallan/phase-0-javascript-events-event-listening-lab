3cf6166b79db12ad42453d022b8bdc4f4520912e
Allan Ratemo <allanisratemo@gmail.com>
2025-09-20 05:37:22 +0300

    Added DOM loader

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+document.addEventListener('DOMContentLoaded', addingEventListener);
+
 function addingEventListener() {
 const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+}
 
-function clickAlert () {
-    alert('I was clicked!');
-}
-input.addEventListener('click', clickAlert);
-}
+// addingEventListener();
